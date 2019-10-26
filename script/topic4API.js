de46e9de4b5e8fe871b9f902adf3af2f43644e95
Olsen - David <49924716+BoriquaRM@users.noreply.github.com>
2019-10-25 23:24:33 -0700

    Add files via upload

--- script/topic4API.js
+++ script/topic4API.js
@@ -11,20 +11,20 @@ weatherRequest.onload = function() {
 
 	/*Temps, Humidity, Wind Speed*/
 	/*This picks out the various temperature readings under the .main array*/
-		document.getElementById('cc-temp').innerHTML = weatherData.main.temp.toFixed(0) + "&deg;F";
-		document.getElementById('cc-lowtemp').innerHTML = weatherData.main.temp_min.toFixed(0);
-		document.getElementById('cc-hightemp').innerHTML = weatherData.main.temp_max.toFixed(0) + "&deg;F";
-		document.getElementById('cc-humidity').innerHTML = weatherData.main.humidity + "%";
-		document.getElementById('cc-windspeed').innerHTML = weatherData.wind.speed.toFixed(0) + " mph";
+		document.getElementById('temp').innerHTML = weatherData.main.temp.toFixed(0) + "&deg;F";
+		document.getElementById('lowtemp').innerHTML = weatherData.main.temp_min.toFixed(0);
+		document.getElementById('hightemp').innerHTML = weatherData.main.temp_max.toFixed(0) + "&deg;F";
+		document.getElementById('humidity').innerHTML = weatherData.main.humidity + "%";
+		document.getElementById('windspeed').innerHTML = weatherData.wind.speed.toFixed(0) + " mph";
 	/*Description of weather*/
 	/*This selects the 'description' of the sky, under the .weather array*/
-		document.getElementById('cc-desc').innerHTML = weatherData.weather[0].description;
+		document.getElementById('desc').innerHTML = weatherData.weather[0].description;
 	/*Weather Icon*/
 	/*This picks out a premade icon on the JSON to reflect a visual for the weather description*/
 		let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
 		let desc = weatherData.weather[0].description;
-		document.getElementById('cc-img').setAttribute('src', icon);
-		document.getElementById('cc-img').setAttribute('alt', desc);
-		document.getElementById('cc-img').setAttribute('title', desc);
+		document.getElementById('img').setAttribute('src', icon);
+		document.getElementById('img').setAttribute('alt', desc);
+		document.getElementById('img').setAttribute('title', desc);
 
 }
